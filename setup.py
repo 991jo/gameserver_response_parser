c72ccf1145ec1ccd6ccf4f83272ab07e3155f4bc
Johannes Erwerle <johannes.erwerle@googlemail.com>
2018-12-26 17:39:29 +0100

    finally fixed the setup.py to display the description

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
 setup(name="gameserver_response_parser",
-        version="0.1.1",
+        version="0.1.2",
         description="a library to parse the responses of gameservers",
+        long_description=readme(),
         url="https://github.com/991jo/gameserver_response_parser",
         author="992jo",
         license="WTF",
