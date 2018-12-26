ad0f577bf1b76e60aff2f17dc011ddd341198a5d
Johannes Erwerle <johannes.erwerle@googlemail.com>
2018-12-26 16:00:45 +0100

    added setup.py for setuptools

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name="gameserver_response_parser",
+        version="0.1",
+        url="https://github.com/991jo/gameserver_response_parser",
+        author="992jo",
+        license="WTF",
+        packages=["gameserver_response_parser"],
+        test_suite="gameserver_response_parser.test.srcds_new_format")
